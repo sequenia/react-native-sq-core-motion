8a2f10ada1cf80383a4d7c9d29db89a102b141a3
Ivan Mikhailovskii <>
2024-07-01 10:26:02 +0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  SqCoreMotionExample
+//
+
+import Foundation
---
+++ ios/SqCoreMotion.swift
@@ -0,0 +1,8 @@
+@objc(SqCoreMotion)
+class SqCoreMotion: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
