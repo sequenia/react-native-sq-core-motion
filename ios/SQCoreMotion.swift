df27c4690271ec280048d553e3c448e3b946e7b8
Ivan Mikhailovskii <>
2024-07-02 10:28:14 +0700

    feat: add sq core motion

---
+++ ios/SQCoreMotion.swift
@@ -0,0 +1,78 @@
+import Foundation
+import React
+
+@objc(SQCoreMotion)
+class SQCoreMotion: RCTEventEmitter {
+
+    public static var emitter: RCTEventEmitter?
+    private var coreMotionManager = SQCoreMotionManager()
+
+    override init() {
+        super.init()
+
+        SQCoreMotion.emitter = self
+    }
+
+    @objc
+    override static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
+
+    @objc
+    override func supportedEvents() -> [String]! {
+      return [
+        "update_step_count",
+        "update_distance",
+        "update_activity_type"
+      ]
+    }
+
+    @objc public func subscribeStepCount(
+            _ resolve: @escaping RCTPromiseResolveBlock,
+            reject: @escaping RCTPromiseRejectBlock
+    ) {
+        self.coreMotionManager
+            .subscribeStepCount { stepCount in
+                SQCoreMotion.emitter?.sendEvent(
+                    withName: "update_step_count",
+                    body: stepCount
+                )
+            } activityTypeHandler: { activityType in
+                SQCoreMotion.emitter?.sendEvent(
+                    withName: "update_activity_type",
+                    body: activityType.rawValue
+                )
+            }
+        resolve(())
+    }
+
+    @objc public func subscribeDistance(
+            _ resolve: @escaping RCTPromiseResolveBlock,
+            reject: @escaping RCTPromiseRejectBlock
+    ) {
+        self.coreMotionManager
+            .subscribeDistance { distance in
+                SQCoreMotion.emitter?.sendEvent(
+                    withName: "update_distance",
+                    body: distance
+                )
+            }
+        resolve(())
+    }
+
+    @objc public func unSubscribeStepCount(
+        _ resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) {
+        self.coreMotionManager.onStop()
+        resolve(())
+    }
+
+    @objc public func unSubscribeDistance(
+        _ resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) {
+        self.coreMotionManager.onStop()
+        resolve(())
+    }
+}
